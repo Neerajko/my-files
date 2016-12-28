e09e9614d706f4964513196766d116b82a433b89
neerajko <neerajko12@gmail.com>
2016-12-28 22:38:15 +0530

    Add files via upload

---
+++ FEED READER TESTING/testing app/spec/AddressBookSpec.js
@@ -0,0 +1,41 @@
+describe('Address Book',function () {
+
+	var addressBook;
+	var thisContact;
+	beforeEach(function(){
+		addressBook=new AddressBook();
+		thisContact=new Contact();		
+	});
+
+
+
+
+
+	it('Should be able to add a contact',function(){
+
+		addressBook.addContact(thisContact);
+		expect(addressBook.getContact(0)).toBe(thisContact);
+	});
+	it('Should be able to delete a contact',function(){
+
+		addressBook.addContact(thisContact);
+		addressBook.deleteContact(0);
+		expect(addressBook.getContact(0)).not.toBeDefined();
+	});
+
+});
+describe('Async Address Book',function(){
+	var addressBook=new AddressBook();
+	beforeEach(function(done){
+		addressBook.getInitialContacts(function(){
+			done();
+		});
+
+	});
+	it('Should grab initial contacts',function(done){
+		
+		addressBook.getInitialContacts();
+		expect(addressBook.initialComplete).toBe(true);
+		done();
+	});
+});
